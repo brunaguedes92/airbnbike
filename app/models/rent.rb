22956e5be40c12674cc554466678cd469f0c3650
Bruna Guedes <bruna.guedes02@gmail.com>
2020-12-05 10:27:27 -0300

    separei componentes

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -3,4 +3,5 @@ class Rent < ApplicationRecord
   belongs_to :bike
 
   validates :user, :bike, :status, :start_date, :end_date, presence: true
+  validates_uniqueness_of :bike, conditions: -> { where.not(status: 'leased') }
 end
