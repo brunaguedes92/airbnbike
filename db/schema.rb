9b9e475d2bf63fc3b4937262d5977286fea0fb91
Bruna Guedes <bruna.guedes02@gmail.com>
2020-12-05 21:28:43 -0300

    removi o campo price da bike

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_213158) do
+ActiveRecord::Schema.define(version: 2020_12_06_002436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2020_12_04_213158) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
-    t.integer "price"
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_bikes_on_user_id"
