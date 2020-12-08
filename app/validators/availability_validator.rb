557abe5f4113922df54406ad84b7237b76d62330
Bruna Guedes <bruna.guedes02@gmail.com>
2020-12-08 17:15:41 -0300

    corrigo o validator

--- app/validators/availability_validator.rb
+++ app/validators/availability_validator.rb
@@ -1,6 +1,6 @@
 class AvailabilityValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    orders = Order.where(["user_id =? AND state=?", record.user_id, 'paid'])
+    orders = Order.where(["bike_id =? AND state=?", record.id, 'paid'])
     date_ranges = orders.map { |b| b.start_date..b.end_date }
 
     date_ranges.each do |range|
