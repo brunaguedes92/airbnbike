e8688b449393c58cd14447b2b7bdde9802521dd8
Bruna Guedes <bruna.guedes02@gmail.com>
2020-12-08 08:28:47 -0300

    flatpicker funcionando bloqueando datas

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,7 +2,11 @@ import flatpickr from 'flatpickr';
 import rangePlugin from 'flatpickr/dist/plugins/rangePlugin';
 
 document.addEventListener('turbolinks:load', () => {
+  const unvailableDates = JSON.parse(document.querySelector('.widget-content').dataset.unavailable)
+  console.log(unvailableDates)
   flatpickr("#start_date", {
+    minDate: 'today',
+    disable: unvailableDates,
     altInput: true,
     plugins: [new rangePlugin({ input: "#end_date"})]
   });
